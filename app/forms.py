c09b72734567ffffffbcc71c449acb41a205f010
Tapashjk <pbsrobotics2023@gmail.com>
2024-09-02 23:26:15 +0545

    loginform backed handel

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,13 @@
+from flask import Flask, render_template, redirect, url_for, flash, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, FileField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, Optional
+from werkzeug.security import generate_password_hash, check_password_hash
 
+app = Flask(__name__)
+app.secret_key = 'your_secret_key'
+
+# Define forms
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
@@ -19,3 +25,55 @@ class LoginForm(FlaskForm):
     remember = BooleanField('Remember Me')
     forgot_password = SubmitField('Forgot Password?')
     submit = SubmitField('Login')
+
+# Dummy user store
+users = {}
+
+@app.route('/')
+def home():
+    return render_template('login.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+        user = users.get(email)
+        if user and check_password_hash(user['password'], password):
+            flash('Login successful!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login failed. Check your email and/or password', 'danger')
+    return render_template('login.html', form=form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        email = form.email.data
+        phone = form.phone.data
+        password = generate_password_hash(form.password.data)
+        profile_picture = form.profile_picture.data
+        bio = form.bio.data
+        terms = form.terms.data
+        
+        if email in users:
+            flash('Email already registered', 'danger')
+        elif not terms:
+            flash('You must agree to the terms and conditions', 'danger')
+        else:
+            users[email] = {
+                'username': username,
+                'phone': phone,
+                'password': password,
+                'profile_picture': profile_picture,
+                'bio': bio
+            }
+            flash('Registration successful! You can now log in', 'success')
+            return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
