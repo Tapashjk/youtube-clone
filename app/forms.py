50a5cf5f97a468ed6dd2b6419b414c020b1ca2b3
Tapashjk <pbsrobotics2023@gmail.com>
2024-09-02 23:24:35 +0545

    added validatord

--- app/forms.py
+++ app/forms.py
@@ -1,14 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, FileField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms import StringField, PasswordField, SubmitField, FileField, BooleanField, TextAreaField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, Optional
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    phone = StringField('Phone Number', validators=[DataRequired(), Length(min=10, max=15)])
-    password = PasswordField('Password', validators=[DataRequired()])
+    phone = StringField('Phone Number', validators=[Optional(), Length(min=10, max=15)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
-    profile_picture = FileField('Profile Picture')
+    profile_picture = FileField('Profile Picture', validators=[Optional()])
+    bio = TextAreaField('Bio', validators=[Optional(), Length(max=200)])
     terms = BooleanField('I agree to the terms and conditions', validators=[DataRequired()])
     submit = SubmitField('Sign Up')
 
@@ -16,4 +17,5 @@ class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
+    forgot_password = SubmitField('Forgot Password?')
     submit = SubmitField('Login')
