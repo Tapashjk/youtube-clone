e692a90e20a81371f648770d8469220bcad4c952
Tapashjk <pbsrobotics2023@gmail.com>
2024-09-02 23:29:30 +0545

    still getting error in redering  form py

--- app/forms.py
+++ app/forms.py
@@ -31,7 +31,7 @@ users = {}
 
 @app.route('/')
 def home():
-    return render_template('login.html')
+    return render_template('templates/login.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
